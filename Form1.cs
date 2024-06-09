4213a31075df8fe09c38703474742c35366bd4d5
POLYDREAMER <Daniil.Kolomieiets@infiz.khpi.edu.ua>
2024-06-09 19:44:46 +0300

    Finish Task number 2

--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Surname_Group_Course_project;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -13,6 +14,7 @@ namespace Kolomieiets_722_a_2_Project_1
     public partial class Form1 : Form
     {
         private bool Mode; // Режим дозволу / заборони введення даних
+        private MajorWork MajorObject; // Створення об'єкта класу MajorWork
         public Form1()
         {
             InitializeComponent();
@@ -25,6 +27,11 @@ namespace Kolomieiets_722_a_2_Project_1
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            About A = new About(); // створення форми About
+
+            A.tAbout.Start();
+            A.ShowDialog(); // відображення діалогового вікна About
+            MajorObject = new MajorWork();
             this.Mode = true;
         }
 
@@ -51,6 +58,9 @@ namespace Kolomieiets_722_a_2_Project_1
                 tClock.Stop();
                 bStart.Text = "Пуск";// зміна тексту на кнопці на "Пуск"
                 this.Mode = true;
+                MajorObject.Write(tbInput.Text);// Запис даних у об'єкт
+                MajorObject.Task();// Обробка даних
+                label1.Text = MajorObject.Read();// Відображення результату
             }
         }
 
---
+++ MajorWork.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Kolomieiets_722_a_2_Project_1
+{
+    class MajorWork
+    {
+        // Вміст робочого об'єкта
+        // Поля
+        private string Data; //вхідні дані
+        private string Result; // Поле результату
+                               // Методи
+        public void Write(string D)// метод запису даних в об'єкт.
+        {
+            this.Data = D;
+        }
+        public string Read()
+        {
+            return this.Result;// метод відображення результату
+        }
+        // У методі Task реалізується завдання: якщо кількість введених цифр більше 5,
+        // то результат = true, інакше False.
+        public void Task() // метод реалізації програмного завдання
+        {
+            if (this.Data.Length > 5)
+            {
+                this.Result = Convert.ToString(true);
+
+            }
+            else
+            {
+                this.Result = Convert.ToString(false);
+            }
+        }
+    }
+}
